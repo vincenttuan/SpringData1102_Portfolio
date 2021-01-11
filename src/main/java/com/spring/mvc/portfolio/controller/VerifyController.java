1a4d62eadbd6465e478d1065ac8145ce18a20471
teacher <teacher@192.168.1.180>
2021-01-11 21:16:54 +0800

    SpringData

--- src/main/java/com/spring/mvc/portfolio/controller/VerifyController.java
+++ src/main/java/com/spring/mvc/portfolio/controller/VerifyController.java
@@ -2,6 +2,7 @@ package com.spring.mvc.portfolio.controller;
 
 import com.spring.mvc.portfolio.entities.Investor;
 import com.spring.mvc.portfolio.repository.InvestorRepository;
+import com.spring.mvc.portfolio.service.PortfolioService;
 import java.util.Optional;
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
@@ -12,6 +13,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/portfolio/verify")
 public class VerifyController {
+    @Autowired
+    PortfolioService service;
+    
+    @GetMapping("/{id}/{code}")
+    @Transactional
+    public String verify(@PathVariable("id") Optional<Integer> id,
+                         @PathVariable("code") Optional<String> code, 
+                         HttpSession session) {
+        String message = "ERROR";
+        Investor investor = service.getInvestorRepository().findOne(id.get());
+        if(investor != null && investor.getCode().equals(code.get())) {
+            service.getInvestorRepository().updatePass(id.get(), Boolean.TRUE);
+            message = "SUCCESS";
+        }
+        session.setAttribute("message", message);
+        return "redirect:/portfolio/verify.jsp";
+    }
     
 }

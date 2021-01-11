ba3484f37ed1f1b3c34873ac7a1c0dd78ef4e43e
teacher <teacher@192.168.1.180>
2021-01-11 19:14:45 +0800

    SpringData

--- src/main/java/com/spring/mvc/portfolio/entities/Investor.java
+++ src/main/java/com/spring/mvc/portfolio/entities/Investor.java
@@ -11,8 +11,112 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+@Entity
 public class Investor implements Serializable {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column
+    private String username;
+    @Column
+    private String password;
+    @Column
+    private String email;
+    @Column
+    private Integer balance; // 投資金額
+    @Column
+    private String code; // email 驗證碼
+    @Column
+    private Boolean pass; // email 驗證碼通過 pass= true
+    
+    @OneToMany
+    private Set<Portfolio> portfolios;
+    
+    @OneToMany
+    private Set<Watch> watchs;
 
+    public Investor() {
+    }
+
+    public Investor(String username, String password, String email, Integer balance) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.balance = balance;
+    }
+    
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Integer balance) {
+        this.balance = balance;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Boolean getPass() {
+        return pass;
+    }
+
+    public void setPass(Boolean pass) {
+        this.pass = pass;
+    }
+
+    public Set<Portfolio> getPortfolios() {
+        return portfolios;
+    }
+
+    public void setPortfolios(Set<Portfolio> portfolios) {
+        this.portfolios = portfolios;
+    }
+
+    public Set<Watch> getWatchs() {
+        return watchs;
+    }
+
+    public void setWatchs(Set<Watch> watchs) {
+        this.watchs = watchs;
+    }
     
     
 }
--- src/main/java/com/spring/mvc/portfolio/entities/Watch.java
+++ src/main/java/com/spring/mvc/portfolio/entities/Watch.java
@@ -15,6 +15,7 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class Watch implements Serializable {
 
     

f022c4949533d04ab8994b5b087e03ecd69d4dc6
teacher <teacher@192.168.1.180>
2021-01-15 21:17:36 +0800

    SpringData

--- src/main/java/com/spring/mvc/portfolio/controller/OrderController.java
+++ src/main/java/com/spring/mvc/portfolio/controller/OrderController.java
@@ -16,8 +16,72 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/portfolio/order")
 public class OrderController {
     
+    @Autowired
+    private InvestorRepository investorRepository;
     
+    @Autowired
+    private TStockRepository tStockRepository;
+    
+    @Autowired
+    private PortfolioRepository portfolioRepository;
+    
+    // 買進
+    @GetMapping(value = {"/buy/{tstock_id}/{amount}"})
+    @Transactional
+    public String buy(HttpSession session, @PathVariable("tstock_id") Integer tstock_id, @PathVariable("amount") Integer amount) {
+        Investor login_investor = (Investor)session.getAttribute("investor");
+        Investor investor = investorRepository.findOne(login_investor.getId());
+        if(investor == null) return "Investor None";
+        TStock ts = tStockRepository.findOne(tstock_id);
+        if(ts == null) return "TStock None";
+        
+        int buyTotalCost = (int)(ts.getPrice().doubleValue() * amount);
+        
+        int balance = investor.getBalance() - buyTotalCost;
+        if(balance < 0) {
+            return "Insufficient balance";
+        }
+        investor.setBalance(balance);
+        
+        Portfolio po = new Portfolio();
+        po.setInvestor(investor);
+        po.settStock(ts);
+        po.setCost(ts.getPrice().doubleValue());
+        po.setAmount(amount);
+        po.setDate(new Date());
+        
+        investorRepository.saveAndFlush(investor);
+        portfolioRepository.saveAndFlush(po);
+        
+        return po.getId() + "";
+    }
+    
+    // 賣出
+    @GetMapping(value = {"/sell/{id}/{amount}"})
+    @Transactional
+    public String sell(HttpSession session, @PathVariable("id") Integer id, @PathVariable("amount") Integer amount) {
+        Investor login_investor = (Investor)session.getAttribute("investor");
+        Investor investor = investorRepository.findOne(login_investor.getId());
+        if(investor == null) return "Investor None";
+        
+        Portfolio po = portfolioRepository.findOne(id);
+        if(po == null) return "Portfolio None";
+        
+        po.setAmount(po.getAmount() - amount);
+        
+        // 回滾利潤
+        int profit = (int)(amount * po.gettStock().getPrice().doubleValue());
+        investor.setBalance(investor.getBalance() + profit);
+        
+        portfolioRepository.saveAndFlush(po);
+        investorRepository.saveAndFlush(investor);
+        
+        return "OK";
+    }
+
     
 }

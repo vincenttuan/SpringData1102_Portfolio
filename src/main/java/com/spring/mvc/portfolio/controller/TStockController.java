fa5cd900a3dfec620521268df83cdfd90b91293b
teacher <teacher@192.168.1.180>
2021-01-15 19:56:31 +0800

    SpringData

--- src/main/java/com/spring/mvc/portfolio/controller/TStockController.java
+++ src/main/java/com/spring/mvc/portfolio/controller/TStockController.java
@@ -15,8 +15,54 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/portfolio/tstock")
 public class TStockController {
 
+    @Autowired
+    private PortfolioService service;
+
+    @PostMapping(value = {"/", "/add"})
+    @Transactional
+    public TStock add(@RequestBody Map<String, String> map) {
+        Classify classify = service.getClassifyRepository().findOne(Integer.parseInt(map.get("classify_id")));
+        TStock ts = new TStock();
+        ts.setName(map.get("name"));
+        ts.setSymbol(map.get("symbol"));
+        ts.setClassify(classify);
+        service.gettStockRepository().save(ts);
+        return ts;
+    }
     
+    @PutMapping(value = {"/", "/update"})
+    @Transactional
+    public TStock update(@RequestBody Map<String, String> map) {
+        Classify classify = service.getClassifyRepository().findOne(Integer.parseInt(map.get("classify_id")));
+        TStock ts = service.gettStockRepository().findOne(Integer.parseInt(map.get("id")));
+        ts.setName(map.get("name"));
+        ts.setSymbol(map.get("symbol"));
+        ts.setClassify(classify);
+        //service.gettStockRepository().update(Long.parseLong(map.get("id")), map.get("name"), map.get("symbol"));
+        return ts;
+    }
+    
+    @DeleteMapping(value = {"/{id}", "/delete/{id}"})
+    @Transactional
+    public Boolean delete(@PathVariable("id") Integer id) {
+        service.gettStockRepository().delete(id);
+        return true;
+    }
+    
+    @GetMapping(value = {"/{id}", "/get/{id}"})
+    @Transactional
+    public TStock get(@PathVariable("id") Integer id) {
+        TStock tStock = service.gettStockRepository().findOne(id);
+        return tStock;
+    }
+
+    @GetMapping(value = {"/", "/query"})
+    public Iterable<TStock> query() {
+        return service.gettStockRepository().findAll();
+    }
 
 }

9a3da0f87c78a272e5dc8c321cef28c9af4780f2
teacher <teacher@192.168.1.180>
2021-01-15 21:29:18 +0800

    SpringData

--- src/main/java/com/spring/mvc/portfolio/controller/ChartController.java
+++ src/main/java/com/spring/mvc/portfolio/controller/ChartController.java
@@ -2,6 +2,7 @@ package com.spring.mvc.portfolio.controller;
 
 import com.spring.mvc.portfolio.repository.AssetRepository;
 import com.spring.mvc.portfolio.repository.ProfitRepository;
+import com.spring.mvc.portfolio.service.PortfolioService;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/portfolio/chart")
 public class ChartController {
+    @Autowired
+    private AssetRepository assetRepository;
     
+    @Autowired
+    private ProfitRepository profitRepository;
     
+    @GetMapping(value = {"/asset/{id}"})
+    public List asset(@PathVariable("id") Optional<Integer> id) {
+        return assetRepository.findById(id.get());
+    }
+    
+    @GetMapping(value = {"/profit/{id}"})
+    public List profit(@PathVariable("id") Optional<Integer> id) {
+        return profitRepository.findById(id.get());
+    }
 }
